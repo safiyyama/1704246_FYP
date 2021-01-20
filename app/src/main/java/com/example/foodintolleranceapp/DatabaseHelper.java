12dacadda97f49a5dc8421d2d59b678161d535ab
safiyyama <1704246@brunel.ac.uk>
2021-01-20 05:34:00 +0000

    Database Helper class created and databse name, table name and column name defined

---
+++ app/src/main/java/com/example/foodintolleranceapp/DatabaseHelper.java
@@ -0,0 +1,28 @@
+package com.example.foodintolleranceapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME="food.db";
+    public static final String TABLE_NAME="food_table";
+    public static final String COLUMN_1 ="Food";
+
+    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
