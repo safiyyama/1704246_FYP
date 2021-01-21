2ac8d26f4b06cade00a7bc0d26eec7fcdf5ee16f
safiyyama <1704246@brunel.ac.uk>
2021-01-21 02:16:44 +0000

    added a method in mainActivity called addData that adds functionality to the AddFood button created so that when the button is pressed the insertData method in the databaseHelper class is run and the users inputs are added to the database. If the input is sucessfully added to the database a toast message is shown to the user telling them their data was added if not they are shown an error message

--- app/src/main/java/com/example/foodintolleranceapp/MainActivity.java
+++ app/src/main/java/com/example/foodintolleranceapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.foodintolleranceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +24,22 @@ public class MainActivity extends AppCompatActivity {
 
         editFood = (EditText)findViewById(R.id.editFood);
         btnAddFood = (Button)findViewById(R.id.btnAddFood);
+
+        AddData();
+    }
+
+    public void AddData(){ //add functionality to the addFood button created on GUI
+        btnAddFood.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData(editFood.getText().toString());
+                        if(isInserted=true)
+                            Toast.makeText(MainActivity.this,"Your food has been added", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this,"There was a problem adding your entry", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 }
