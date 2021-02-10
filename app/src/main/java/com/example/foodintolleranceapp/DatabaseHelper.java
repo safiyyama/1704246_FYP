3e78837cded1d5227c9c12cce54468e218a626de
safiyyama <1704246@brunel.ac.uk>
2021-02-10 16:14:29 +0000

    added new symptom table and insertSymptom data method to the databsehelper class so that symptom data can be added in. Added functionality to the fields and buttons on symptom page so that symptoms scores entered are added to the database, did a check to tell user if symptoms were added to the database successfully.

--- app/src/main/java/com/example/foodintolleranceapp/DatabaseHelper.java
+++ app/src/main/java/com/example/foodintolleranceapp/DatabaseHelper.java
@@ -11,11 +11,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME="food_intolerance.db";
     public static final String TABLE_NAME="food_eaten";
+    public static final String TABLE2_NAME ="symptoms";
     public static final String COLUMN_1 ="Date";
     public static final String COLUMN_2 ="Breakfast";
     public static final String COLUMN_3 ="Lunch";
     public static final String COLUMN_4 ="Dinner";
     public static final String COLUMN_5 ="Snack";
+    //Columns for symptom table
+    public static final String COL_DATE = "Date";
+    public static final String COL_NAUSEA= "Nausea";
+    public static final String COL_STOMACH = "Stomach Pains";
+    public static final String COL_BLOAT = "Bloat/Gas";
+    public static final String COL_HEART = "Heartburn";
+    public static final String COL_SKIN = "Skin Rashes/Itching";
+    public static final String COL_RATING = "Symptom Rating";
 
 
     public DatabaseHelper(@Nullable Context context) //when this constructor is called the database is created
@@ -27,12 +36,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) { //Here we can execute an SQL statements like create table
         db.execSQL("create table " + TABLE_NAME +" (DATE TEXT, BREAKFAST TEXT, LUNCH TEXT, DINNER TEXT, SNACK TEXT)");
+        db.execSQL("create table " + TABLE2_NAME +" (DATE TEXT, NAUSEA INTEGER  , STOMACH INTEGER, BLOAT INTEGER, HEART INTEGER, SKIN INTEGER, RATING INTEGER)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {//this drops the table if there are upgrade issues
 
         db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE2_NAME);
         onCreate(db);
 
     }
@@ -44,8 +55,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         contentValues.put(COLUMN_2, breakfast);
         contentValues.put(COLUMN_3, lunch);
         contentValues.put(COLUMN_4, dinner);
-        contentValues.put(COLUMN_5, dinner);
+        contentValues.put(COLUMN_5, snack);
         long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result==-1)
+            return false;
+        else
+            return true;
+    }
+
+    public boolean insertSymptomData(String date, String nausea, String stomach, String bloat, String heart, String skin, String rating){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_DATE, date);
+        contentValues.put(COL_NAUSEA, nausea);
+        contentValues.put(COL_STOMACH, stomach);
+        contentValues.put(COL_BLOAT, bloat);
+        contentValues.put(COL_HEART, heart);
+        contentValues.put(COL_SKIN, skin);
+        contentValues.put(COL_RATING, rating);
+        long result = db.insert(TABLE2_NAME, null, contentValues);
         if(result==-1)
             return false;
         else
--- app/src/main/java/com/example/foodintolleranceapp/SymptomPage.java
+++ app/src/main/java/com/example/foodintolleranceapp/SymptomPage.java
@@ -3,12 +3,65 @@ package com.example.foodintolleranceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class SymptomPage extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+
+    EditText editNausea, editStomach, editBloat, editHeart, editSkin;
+    TextView textRating;
+    Button btnAddSymptoms;
+    String currentDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_symptom_page);
+
+        myDb = new DatabaseHelper(this);
+
+        Calendar calendar = Calendar.getInstance();
+        currentDate = DateFormat.getDateInstance().format(calendar.getTime());
+
+        editNausea = (EditText)findViewById(R.id.editNausea);
+        editStomach = (EditText)findViewById(R.id.editStomach);
+        editBloat = (EditText)findViewById(R.id.editBloat);
+        editHeart = (EditText)findViewById(R.id.editHeart);
+        editSkin = (EditText)findViewById(R.id.editSkin);
+
+        textRating = (TextView) findViewById(R.id.textRating);
+
+        btnAddSymptoms = (Button)findViewById(R.id.btnAddSymptoms);
+
+        AddSymptomData();
+    }
+
+    public void AddSymptomData(){
+        btnAddSymptoms.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertSymptomData(currentDate,
+                                editNausea.getText().toString(),
+                                editStomach.getText().toString(),
+                                editBloat.getText().toString(),
+                                editHeart.getText().toString(),
+                                editSkin.getText().toString(),
+                                textRating.getText().toString());
+                        if(isInserted=true)
+                            Toast.makeText(SymptomPage.this,"Your food entry has been added", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(SymptomPage.this,"There was a problem adding your entry", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 }
